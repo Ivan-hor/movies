c6f51931df54a34a5a10c2098cba596d9a3ac9cc
Ol_Kuts <kuts.ol.vas@gmail.com>
2021-07-09 11:43:46 +0300

    feat/task-11/Wrapper and Header and Main

--- src/pages/greeting.tsx
+++ src/pages/greeting.tsx
@@ -1,12 +1,18 @@
 // Core
 import { Typography } from 'antd';
 
+import { SectionWrapper, MainWrapper } from '../components/styles';
+import { Header } from '../components';
+
 const { Title } = Typography;
 
 export const Greeting = () => {
     return (
         <>
-            <Title>Групповой проект</Title>
+            <SectionWrapper className = 'ant-layout'>
+                <Header />
+                <MainWrapper className = 'ant-layout-content' />
+            </SectionWrapper>
         </>
     );
 };
