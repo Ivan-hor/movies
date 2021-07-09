443856bc38a8bec96c9446f6d3f5240fe60f5852
Ol_Kuts <kuts.ol.vas@gmail.com>
2021-07-09 14:15:58 +0300

    feat/task-13/Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer
+            className = 'ant-layout-footer'
+            style = { { marginTop: '25px' } }>
+            <p>©2021 — Lectrum LLC</p>
+        </footer>
+    );
+};
