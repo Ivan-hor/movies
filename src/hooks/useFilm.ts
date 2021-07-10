406d69b47f531e31f35f335619a0342d645e27fa
Elena Pavlova <lenlho22@gmail.com>
2021-07-10 10:19:25 +0300

    Add useApiError hook to handle error #4

--- src/hooks/useFilm.ts
+++ src/hooks/useFilm.ts
@@ -2,19 +2,16 @@ import { useQuery, UseQueryResult } from 'react-query';
 
 import { api } from '../api';
 import { IFilmDetailsModel, ErrorType } from '../types';
-import { useStore } from './useStore';
+import { useApiError } from './useApiError';
 
 export const useFilm = (filmId: string): UseQueryResult<IFilmDetailsModel, ErrorType> => {
-    const { uiStore } = useStore();
-    const { setError } = uiStore;
+    const { handleError } = useApiError();
 
     const query = useQuery<IFilmDetailsModel, ErrorType>(
         ['filmById', filmId],
         () => api.getMovie(filmId),
         {
-            onError: (error: ErrorType) => {
-                setError(error.message);
-            },
+            onError: handleError,
         },
     );
 
