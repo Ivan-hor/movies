a316324f3d5c258eed2ec983cdd5b09e3d6946eb
i007300@yandex.ru <i007300@yandex.ru>
2021-07-14 01:03:49 -0700

    #2 pagination done

--- src/components/TopRatedFilmsTable.tsx
+++ src/components/TopRatedFilmsTable.tsx
@@ -8,10 +8,12 @@ import { KeysOfList } from '../types';
 
 export const TopRatedFilmsTable = () => {
     const [page, setPage] = useState(1);
+    // const [loading, setLoading] = useState(false);
     const query = useTopRatedFilms(page);
 
     const onChange = (newPage: number) => {
         setPage(newPage);
+        // setLoading(true);
     };
 
     const columns = getColumns([
@@ -25,13 +27,13 @@ export const TopRatedFilmsTable = () => {
         KeysOfList.details,
     ]);
     if (query.isFetched) {
-        console.log(query.data);
-
         return <Table
             rowKey = 'id'
             columns = { columns }
             dataSource = { query.data }
-            pagination = { { onChange, total: 1000, showSizeChanger: false } } />;
+            pagination = { {
+                onChange, total: 2000, showSizeChanger: false, pageSize: 20, current: page,
+            } } />;
     }
 
     return <>loading</>;
--- src/hooks/useTopRatedFilms.ts
+++ src/hooks/useTopRatedFilms.ts
@@ -4,6 +4,6 @@ import { api } from '../api';
 
 export const useTopRatedFilms = (page: number) => {
     return  (
-        useQuery(['movies', 'top-rated'], () => api.getTopRated(page))
+        useQuery(['top-rated', page], () => api.getTopRated(page))
     );
 };
